4f4e71d592fab655e8db992d23bfa649d688e3d1
moro <62976395+morogoha1995@users.noreply.github.com>
2020-05-07 22:37:57 +0900

    Added assets.

--- src/scenes/boot.ts
+++ src/scenes/boot.ts
@@ -3,6 +3,21 @@ class Boot extends Phaser.Scene {
     super({ key: "boot" })
   }
 
+  preload() {
+    this.load
+      .image("house", "assets/imgs/house.png")
+      .image("x", "assets/imgs/x.png")
+      .image("tile", "assets/imgs/tile.jpg")
+      .image("field", "assets/imgs/tile.jpg")
+      .image("stone", "assets/imgs/player/stone.png")
+      .image("arrow", "assets/imgs/player/arrow.png")
+      .image("rifle", "assets/imgs/player/rifle.png")
+      .image("soldier", "assets/imgs/enemy/soldier.png")
+      .image("boss", "assets/imgs/enemy/boss.png")
+      .image("golem", "assets/imgs/enemy/golem.png")
+      .image("god", "assets/imgs/enemy/god.png")
+  }
+
   create() {
     this.scene.start("game")
   }
