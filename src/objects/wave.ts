8b8e631f4666f9eee05510bc563ecb41352a08aa
moro <62976395+morogoha1995@users.noreply.github.com>
2020-05-09 21:44:50 +0900

    Added a comments on Wave.detemineEnemyName().

--- src/objects/wave.ts
+++ src/objects/wave.ts
@@ -48,8 +48,7 @@ export class Wave {
   }
 
   private determineEnemyName(): EnemyName {
-    const isStrong = this.spawnCount % 3 === 0
-
+    // 上から弱い順。
     const enemyNames: EnemyName[] = [
       "soldier",
       "ghost",
@@ -57,11 +56,13 @@ export class Wave {
       "god"
     ]
 
+    // enemyNamesからどの名前を取り出すかの値が代入される。
+    // 5ウェーブごとに取り出される敵の位が上がる。
     let enemyIndex = Math.floor(this.current / 5)
-
-    if (isStrong)
+    // 生成数が3で割り切れる数の場合、1つ上位の敵を生成する
+    if (this.spawnCount % 3 === 0)
       enemyIndex++
-
+    // enemyNamesの要素数を超えないように低い方を代入し直す。
     enemyIndex = Math.min(enemyIndex, enemyNames.length)
 
     return enemyNames[enemyIndex]
