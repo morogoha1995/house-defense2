99d302e0b60653807bf7aad34815d642195df3bf
moro <62976395+morogoha1995@users.noreply.github.com>
2020-05-16 13:25:03 +0900

    Added Line to Shootable.

--- src/objects/weapon/shootable.ts
+++ src/objects/weapon/shootable.ts
@@ -8,6 +8,7 @@ export class Shootable extends Weapon {
   private interval: number
   private nextAttack: number
   private bullet: Phaser.GameObjects.Image
+  private ballistic: Phaser.GameObjects.Line
 
   constructor(scene: Phaser.Scene, x: number, y: number, name: ShootableName) {
     super(scene, x, y, name)
@@ -18,6 +19,9 @@ export class Shootable extends Weapon {
     this.bullet = scene.add.image(x, y, `${name}Bullet`)
       .setVisible(false)
       .setOrigin(0.5, 0)
+    this.ballistic = scene.add.line(0, 0, 0, 0, 0)
+      .setVisible(false)
+      .setStrokeStyle(1, 0x333333)
   }
 
   update(e: Enemy) {
@@ -28,22 +32,37 @@ export class Shootable extends Weapon {
 
   // TODO
   attack(e: Enemy) {
-    const x = e.body.center.x
-    const y = e.body.center.y
-    const bullet = this.bullet
-    const rad = Phaser.Math.Angle.Between(this.x, this.y, x, y)
-    const deg = Phaser.Math.RadToDeg(rad) + 90
-    bullet.setPosition(x, y).setAngle(deg).setAlpha(1).setVisible(true)
-
     e.damaged(this.atk)
 
-    const animationTime = this.interval / 3
+    const x = e.body.center.x
+    const y = e.body.center.y
+    const rad = Phaser.Math.Angle.Between(this.x, this.y, x, y)
+    const deg = Phaser.Math.RadToDeg(rad) + 90
+
+    const b = this.bullet
+    b
+      .setPosition(x, y)
+      .setAngle(deg)
+      .setAlpha(1)
+      .setVisible(true)
+      .setDepth(5)
+
+    const ba = this.ballistic
+    ba
+      .setTo(this.x, this.y, x, y)
+      .setAlpha(0.6)
+      .setVisible(true)
+
+    const animationTime = this.interval / 4
     this.scene.add.tween({
-      targets: bullet,
+      targets: [b, ba],
       delay: animationTime,
       alpha: 0,
       duration: animationTime,
-      onComplete: () => bullet.setVisible(false)
+      onComplete: () => {
+        b.setVisible(false)
+        ba.setVisible(false)
+      }
     })
 
     this.nextAttack = this.scene.time.now + this.interval
