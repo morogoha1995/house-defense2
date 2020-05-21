f7fa6ee8b9b3fbb238b0a293ad45cdd780f1920f
moro <62976395+morogoha1995@users.noreply.github.com>
2020-05-21 20:55:57 +0900

    Improved a process that check overlap selectedWeapon to tiles.

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -31,15 +31,13 @@ class Game extends Phaser.Scene {
     this.weaponGroup = new WeaponGroup(this)
     this.selectedWeapon = new SelectedWeapon(this)
     this.field.bg
-      .on("pointermove", (e: any) => {
-        this.moveSelectedWeapon(e.x, e.y)
-      })
+      .on("pointermove", (e: any) => this.moveSelectedWeapon(e.x, e.y))
       .on("pointerdown", (e: any) => this.putWeapon(e.x, e.y))
 
     this.shop = new Shop(this)
 
     for (const key in this.shop.weapons) {
-      const name = key as WeaponName
+      const name = <WeaponName>key
       this.shop.weapons[name].on("pointerdown", (e: any) => this.buyWeapon(name))
     }
 
@@ -53,7 +51,6 @@ class Game extends Phaser.Scene {
     this.checkEnemyDeath()
     this.checkWave()
 
-    this.selectedWeapon.update()
     this.wave.update(this.time.now, this.field.route)
     this.weaponGroup.update(this.wave.enemyGroup)
   }
@@ -70,6 +67,9 @@ class Game extends Phaser.Scene {
   }
 
   private moveSelectedWeapon(x: number, y: number) {
+    if (!this.selectedWeapon.getIsSelected())
+      return
+
     this.selectedWeapon.setPosition(x, y)
     const sw: any = this.selectedWeapon.getWeapon()
 
@@ -79,25 +79,15 @@ class Game extends Phaser.Scene {
 
     this.selectedWeapon.setIsOverlap("toWeapon", this.physics.overlap(sw, this.weaponGroup))
 
-    const top = Math.floor(sw.body.top / TILE_SIZE)
-    const right = Math.floor(sw.body.right / TILE_SIZE)
-    const bottom = Math.floor(sw.body.bottom / TILE_SIZE)
-    const left = Math.floor(sw.body.left / TILE_SIZE)
+    const tiles = this.field.layer.getTilesWithinWorldXY(Math.floor(sw.body.left), Math.floor(sw.body.top), sw.body.width, sw.body.height)
+    let isOverlapToRoute = false
+    for (const t of tiles) {
+      isOverlapToRoute = t.index === 2 || t.index === 1
+      if (isOverlapToRoute)
+        break
+    }
 
-    const tile = this.field.layer.getTileAt(left, bottom)
-
-    const tile2 = this.field.layer.getTileAt(right, bottom)
-
-    const tile3 = this.field.layer.getTileAt(left, top)
-
-    const tile4 = this.field.layer.getTileAt(right, top)
-
-    if (!tile || !tile2 || !tile3 || !tile4)
-      return
-
-    const v = tile.index !== 0 || tile2.index !== 0 || tile3.index !== 0 || tile4.index !== 0
-
-    this.selectedWeapon.setIsOverlap("toRoute", v)
+    this.selectedWeapon.setIsOverlap("toRoute", isOverlapToRoute)
 
     const alpha = this.selectedWeapon.getIsOverlap() ? 0.3 : 1
     this.selectedWeapon.setAlpha(alpha)
@@ -109,9 +99,14 @@ class Game extends Phaser.Scene {
   }
 
   private putWeapon(x: number, y: number) {
-    this.selectedWeapon.removeAll(true)
+    if (!this.selectedWeapon.getIsSelected())
+      return
+
+    this.selectedWeapon.rm()
     if (this.selectedWeapon.getIsOverlap())
       return
+
+
 
     const name = <WeaponName>this.selectedWeapon.name
 
